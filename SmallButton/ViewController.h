a483a16a16f0703b6d95da009bdbbfd39dfffc79
Brian Bal <briantbal@gmail.com>
2012-09-18 11:03:40 -0400

    Initial Commit

---
+++ SmallButton/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SmallButton
+//
+//  Created by Brian Bal on 9/18/12.
+//  Copyright (c) 2012 UITricks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ SmallButton/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SmallButton
+//
+//  Created by Brian Bal on 9/18/12.
+//  Copyright (c) 2012 UITricks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
